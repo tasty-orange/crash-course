4a07e28c386145681ed0562bd65f27c948e9e6f9
tasty-orange <gwendal.piemontesi@heig-vd.ch>
2024-10-04 14:03:29 +0200

    first

---
+++ example-uptime/src/main/java/ch/heigvd/amt/ProbeResource.java
@@ -0,0 +1,22 @@
+package ch.heigvd.amt;
+
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/")
+public class ProbeResource {
+
+    @Inject
+    Template indexPage;
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance index() {
+        return indexPage.instance();
+    }
+}
---
+++ example-uptime/src/test/java/ch/heigvd/amt/ProbeResourceIT.java
@@ -0,0 +1,8 @@
+package ch.heigvd.amt;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ProbeResourceIT extends ProbeResourceTest {
+    // Execute the same tests but in packaged mode.
+}
